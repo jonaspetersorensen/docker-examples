d82a7b1919e4e6188ab77fb8238b3992449c7861
Jonas Peter Sørensen <1851498+jonaspetersorensen@users.noreply.github.com>
2021-09-17 19:04:45 +0200

    Too much trouble to keep up with javascript security issues

--- webpack/src/someotherstuff/index.js
+++
@@ -1,9 +0,0 @@
-function component() {
-   const element = document.createElement('div');
-   element.innerHTML = "This text was injected by ./src/someotherstuff/index.js";
-   return element;
- }
- 
- window.addEventListener('load', function() {
-   document.body.appendChild(component());
- })
